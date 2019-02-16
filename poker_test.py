2cedaa7a92a493633f6e63e358a8bb0f741d97fa
Tricerintops <erinvh620@gmail.com>
2019-02-16 22:45:03 +0000

    Moved all unit tests over to poker_test.py

--- poker_test.py
+++ poker_test.py
@@ -11,6 +11,42 @@ class TestPoker(TestCase):
         hand = ["2c", "4c", "4c", "5d", "6c"]
         self.assertEqual(hand_strength(hand), 1)
 
+    def test_hand_strength_with_two_pair(self):
+        hand = ["2c", "2d", "4c", "4d", "6c"]
+        self.assertEqual(hand_strength(hand), 2)
+
+    def test_hand_strength_with_three_of_a_kind(self):
+        hand = ["2c", "4c", "4c", "4d", "6c"]
+        self.assertEqual(hand_strength(hand), 3)
+
+    def test_hand_strength_with_straight_ace_to_5(self):
+        hand = ["14d", "2c", "3c", "4c", "5c"]
+        self.assertEqual(hand_strength(hand), 4)
+
+    def test_hand_strength_with_straight_10_to_ace(self):
+        hand = ["14d", "10c", "11c", "12c", "13c"]
+        self.assertEqual(hand_strength(hand), 4)
+
+    def test_hand_strength_with_flush(self):
+        hand = ["2c", "10c", "11c", "12c", "13c"]
+        self.assertEqual(hand_strength(hand), 5)
+
+    def test_hand_strength_with_full_house(self):
+        hand = ["14d", "14c", "14s", "12c", "12d"]
+        self.assertEqual(hand_strength(hand), 6)
+
+    def test_hand_strength_with_four_of_a_kind(self):
+        hand = ["14d", "14c", "14s", "14h", "13c"]
+        self.assertEqual(hand_strength(hand), 7)
+
+    def test_hand_strength_with_straight_flush_ace_to_5(self):
+        hand = ["14c", "2c", "3c", "4c", "5c"]
+        self.assertEqual(hand_strength(hand), 8)
+
+    def test_hand_strength_with_straight_flush_10_to_ace(self):
+        hand = ["14c", "10c", "11c", "12c", "13c"]
+        self.assertEqual(hand_strength(hand), 8)
+
 
 if __name__ == '__main__':
     unittest.main()
