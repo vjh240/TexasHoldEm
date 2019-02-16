308b2291ce379bf3ff73bef13d81a1abf4c377f7
Tricerintops <erinvh620@gmail.com>
2019-02-16 22:15:39 +0000

    Added 2 *real* unit tests to poker.py file

--- poker.py
+++ poker.py
@@ -207,3 +207,14 @@ unit_test_hands()
 
 # a possible opportunity to remove iterations:
 # if the players best hand is the river, they will tie with all other hand possibilities in which the river is their best hand
+
+from unittest import TestCase
+
+class TestPoker(TestCase):
+    def test_hand_strength_with_high_card(self):
+        hand = ["2c", "4c", "6d", "8d", "10s"]
+        self.assertEqual(hand_strength(hand), 0)
+
+    def test_hand_strength_with_one_pair(self):
+        hand = ["2c", "4c", "4c", "5d", "6c"]
+        self.assertEqual(hand_strength(hand), 1)
