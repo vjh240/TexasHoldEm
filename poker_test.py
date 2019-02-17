5b1e26a251cb6101f941532ca9424c1cc8246460
Vincent Hamalainen <vjh@stern.nyu.edu>
2019-02-16 19:18:33 -0500

    made unit test class name specific

--- poker_test.py
+++ poker_test.py
@@ -2,7 +2,7 @@ from poker import hand_strength
 import unittest
 from unittest import TestCase
 
-class TestPoker(TestCase):
+class TestHandStrength(TestCase):
     def test_hand_strength_with_high_card(self):
         hand = ["2c", "4c", "6d", "8d", "10s"]
         self.assertEqual(hand_strength(hand), 0)
